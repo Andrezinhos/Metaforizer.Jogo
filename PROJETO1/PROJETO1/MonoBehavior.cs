4e026ccd04e0f049c82eb69e95ada9a21f42e1d3
andrezinhos <andre.mbmagon@CAR0717611W11-1>
2025-07-22 15:57:36 -0300

    Criado o MonoBehavior

---
+++ PROJETO1/PROJETO1/MonoBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public abstract class MonoBehavior
+    {
+        private Thread t;
+        private bool ativo = true;
+
+        public void Run()
+        {
+            Awake();
+            Start();
+
+            t = new Thread(
+                () => {
+                    while (ativo)
+                    {
+                        Update();
+                        LateUpdate();
+                        Thread.Sleep(800);
+                    }
+
+                    OnDestroy();
+
+                }
+            );
+
+            t.Start();
+        }
+
+        public void Stop()
+        {
+            this.ativo = false;
+            t.Join();
+        }
+
+        public void Awake() { }
+        public void Start() { }
+        public void Update() { }
+        public  void LateUpdate() { }
+        public void OnDestroy() { }
+    }
+}
